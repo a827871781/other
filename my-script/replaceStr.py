ddaf16c083b2c154b82ffe4f0d7d1dff021ec6d5
syz <a827871781@gmail.com>
2019-11-21 15:20:00 +0800

    字符串替换脚本修改

--- my-script/replaceStr.py
+++ my-script/replaceStr.py
@@ -1,16 +1,19 @@
 #!/usr/bin/python3
 # encoding: utf-8
 import json
-import sys
 import pyperclip
 def main():
-    str = sys.argv[1]
+    str = pyperclip.paste()
+    if not'[' in str and not']' in str:
+        result = {"items": [{"title": "当前剪切板内字符串没有 [ / ] ,请重新复制." }]}
+        print(json.dumps(result))
+        return
+
     str1 = str.replace('[','{')
     str1 = str1.replace(']','}')
     result = {"items": [{"title": str1},{"title":str}]}
     print(json.dumps(result))
     pyperclip.copy(str1)
-
 
 if __name__ == '__main__':
     main()
